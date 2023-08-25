0d2d2c1240900ed22844ae4d5bc16c70924ad90b
Andreska <andreska_nayara@hotmail.com>
2023-08-25 11:53:13 -0300

    Ultima versao

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,20 @@
+import styles from '../Home/Home.module.css'
+import logo from '../../assets/img-logo.png'
+import { FaBeer } from 'react-icons/fa';
+
 export default function Home() {
     return(
         <>
+            <header>
+                <div className={styles.info_header}>
+                    <div className={styles.logo}>
+                        <a href="#"><img src={logo} alt="logo"/></a>
+                    </div>
+                    <div className={styles.icon_header}>
+                        <i class='fa-solid fa-magnifying-glass'></i>
+                    </div>
+                </div>
+            </header>
         </>
     )
 
